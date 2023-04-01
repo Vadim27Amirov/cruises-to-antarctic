6d60650f78a86cdac3d8e800a9ca5f60a281c5b7
Vadim <amirovadim30@mail.ru>
2023-04-01 19:41:59 +0300

    Добавляет файл map

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {mobileMenu} from './modules/mobile-menu.js';
+import {initMap} from './modules/map.js';
 
 // ---------------------------------
 
@@ -12,6 +13,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   iosVhFix();
   mobileMenu();
+  initMap();
 
   // Modules
   // ---------------------------------
